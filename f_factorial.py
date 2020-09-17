e5e42f1ec6b6770175b0cb42b2f1f99fbc7461f5
James Berardini <jberardini2@yahoo.com>
2020-09-16 17:19:14 -0700

    Update f_factorial.py
    variable renaming

--- f_factorial.py
+++ f_factorial.py
@@ -3,8 +3,8 @@ def computeFactorial(num):
 	# for help on docstring: "help f_facorial.py"
 	""" Returns factorial result.
 	    Returns factorial computed 1 to <num> for parameter <num>."""
-	int_result = 1
+	int_fact = 1
 	x = 1
 	for x in range(x, num + 1):
-		int_result = x * int_result 
-	return int_result
+		int_fact = x * int_result 
+	return int_fact
